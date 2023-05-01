8a7e8dd8a0e2e3138cd5022c697d7b4fdfdb9b72
Juan <jcarriola83@gmail.com>
2023-04-30 19:12:12 -0700

    function for scraping the url with Puppeteer implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,54 @@
 import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [url, setURL] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setURL("");
+    sendURL();
+  };
+
+  async function sendURL() {
+    try {
+      const request = await fetch("http://localhost:8080/api/url", {
+        method: "POST",
+        body: JSON.stringify({
+          url,
+        }),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await request.json();
+      if (data.message) {
+        setLoading(false);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
-    <div className="App">
-      <p>Hello World!</p>
+    <div className="home">
+      <form className="home__form">
+        <h2>Website Aggregator</h2>
+        <label htmlFor="url">Provide the website URL</label>
+        <input
+          type="url"
+          name="url"
+          id="url"
+          value={url}
+          onChange={(e) => setURL(e.target.value)}
+        />
+        <button onClick={handleSubmit}>ADD WEBSITE</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import { createRequire } from "module";
+
 const require = createRequire(import.meta.url);
+const puppeteer = require("puppeteer");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -13,6 +15,32 @@ app.get("/api", (req, res) => {
   res.json({
     message: "Hello world",
   });
+});
+
+app.post("/api/url", (req, res) => {
+  const { url } = req.body;
+
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const websiteContent = await page.evaluate(() => {
+      return document.documentElement.innerText.trim();
+    });
+
+    const websiteOgImage = await page.evaluate(() => {
+      const metas = document.getElementsByTagName("meta");
+      for (let i = 0; i < metas.length; i++) {
+        if (metas[i].getAttribute("property") === "og:image") {
+          return metas[i].getAttribute("content");
+        }
+      }
+    });
+
+    console.log({ websiteContent, websiteOgImage });
+    await browser.close();
+  })();
 });
 
 app.listen(PORT, () => {
